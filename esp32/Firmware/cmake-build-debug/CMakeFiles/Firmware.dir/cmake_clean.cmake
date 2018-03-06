6a5d86cd79d9932b71e1e7940c3879775b2b9fec
FX <fx.boisserin@gmail.com>
2018-03-07 00:13:45 +0100

    samples

---
+++ esp32/Firmware/cmake-build-debug/CMakeFiles/Firmware.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Firmware.dir/TFTLib.cpp.o"
+  "Firmware.pdb"
+  "Firmware.exe"
+  "libFirmware.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Firmware.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
